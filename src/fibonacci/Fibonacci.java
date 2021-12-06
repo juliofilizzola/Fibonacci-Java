f30badf4b2aeacf047a3267a9e6af58e435643c3
Filizzola <juliofilizzola@hotmail.com>
2021-12-06 17:00:22 -0300

    BUILD: Finalizando metodo Fibonacci

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -5,7 +5,17 @@ import java.util.ArrayList;
 
 public class Fibonacci {
   public static List<Integer> fibonacci() {
-    
+    List<Integer> array = new ArrayList<Integer>();
+    array.add(0);
+    array.add(1);
+
+    for (int index = 1;; index +=1) {
+      if (array.get(index) >= 350) {
+        break;
+      }
+      array.add(array.get(index) + array.get(index -1));
+    }
+    return array;
   }
 
 }
