9af9e602867128a24974e51bfb32944b39f8a834
Filizzola <juliofilizzola@hotmail.com>
2021-12-06 17:26:55 -0300

    BUILD: chamando metodos no main Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,11 @@
+import fibonacci.Fibonacci;
+
 public class Principal {
   public static void main(String[] args) {
+    System.out.println(Fibonacci.fibonacci());
+    System.out.println(Fibonacci.validFibonacci(5));
+    System.out.println(Fibonacci.validFibonacci(1));
+    System.out.println(Fibonacci.validFibonacci(4));
 
   }
 }
